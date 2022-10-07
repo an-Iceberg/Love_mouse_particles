1f8b6dac024fbbea8f217604b44bc4d5a2caaf6e
an-Iceberg <a.g.marosi@bluewin.ch>
2022-10-07 23:38:43 +0200

    first commit

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(game)
+  game.window.width = 1280
+  game.window.height = 820
+  game.window.x = 300
+  game.window.y = 100
+  game.window.resizable = false
+  game.window.vsync = false
+  game.window.msaa = 0
+  game.window.title = "particles"
+end
---
+++ main.lua
@@ -0,0 +1,31 @@
+math.randomseed(os.time())
+
+local mouse_trail = require("utils.particles")
+
+function love.load()
+end
+
+function love.mousepressed(x, y, button)
+  if button == 1 or button == 2 then
+    mouse_trail:add(x, y)
+  end
+end
+
+function love.mousemoved(x, y, delta_x, delta_y)
+  mouse_trail:add(x, y)
+end
+
+function love.wheelmoved(x, y)
+  mouse_trail:modify_amount(y)
+end
+
+function love.update(delta_time)
+  mouse_trail:update(delta_time)
+end
+
+function love.draw()
+  mouse_trail:draw()
+
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.print("Amount: "..mouse_trail.amount.."\nParticles: "..#mouse_trail.particles, 5, 5)
+end
